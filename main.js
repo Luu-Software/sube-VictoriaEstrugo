52f041c053033bb67426e7b34730e67a3da0bc73
VictoriaEstrugo <victoriaestrugo2011@gmail.com>
2026-05-13 15:26:35 -0300

    SUBE TERMINADO

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 6-12 km	    | $ 855.97		          | $ 1360.99
 12-27 km	  | $ 917.24		          | $ 1458.41
 */
-let boton = document.getElementById("boton");
 
 boton.addEventListener("click", function () {
 
@@ -20,11 +19,38 @@ boton.addEventListener("click", function () {
 
  if (reg === "si"){
   if (km > 0 && km < 3){
-    precio = "$ 715-24":
+    precio = "$ 715.24";
   }
   else if (km > 3 && km < 6) {
     precio = "794.74"
   }
- }
+  else if (km > 6 && km < 12) {
+    precio = "855.97"
+  } 
+  else if (km > 12 && km < 27) {
+    precio = "917.24"
+  }
+  else {
+    precio = "error"
+  }
+  }
 
+  if (reg === "no") {
+    if (km > 0 && km < 3) {
+      precio = "1137.23"
+  }
+  else if (km > 3 && km < 6) {
+    precio = "1263.64"
+    }
+    else if (km > 6 && km < 12) {
+      precio = "1360.99"
+    }
+    else if (km > 12 && km <= 27) {
+      precio = "1458.41"
+    }
+    else {
+      precio = "error"
+    }
+  }
+tarifa.innerText = precio
 });
