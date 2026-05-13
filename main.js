b04bb7090148d0dbede9af45a00d89584937d990
VictoriaEstrugo <victoriaestrugo2011@gmail.com>
2026-05-13 15:56:00 -0300

    SUBE MANIJA TERMINADO

--- main.js
+++ main.js
@@ -12,11 +12,14 @@ boton.addEventListener("click", function () {
 
  let reg;
  let km;
+ let lugar;
+ let precio;
 
  reg = registrada.value;
  km = distancia.value;
- let precio;
+lugar = colectivo.value;
 
+ if (lugar === "buenos aires"){
  if (reg === "si"){
   if (km > 0 && km < 3){
     precio = "$ 715.24";
@@ -52,5 +55,83 @@ boton.addEventListener("click", function () {
       precio = "error"
     }
   }
+  }
+  if (lugar === "provincia"){
+    if (reg === "si"){
+     if (km > 0 && km < 3){
+       precio = "$ 715.24";
+     }
+     else if (km > 3 && km < 6) {
+       precio = "794.74"
+     }
+     else if (km > 6 && km < 12) {
+       precio = "855.97"
+     } 
+     else if (km > 12 && km < 27) {
+       precio = "917.24"
+     }
+     else {
+       precio = "error"
+     }
+     }
+   
+     if (reg === "no") {
+       if (km > 0 && km < 3) {
+         precio = "1137.23"
+     }
+     else if (km > 3 && km < 6) {
+       precio = "1263.64"
+       }
+       else if (km > 6 && km < 12) {
+         precio = "1360.99"
+       }
+       else if (km > 12 && km <= 27) {
+         precio = "1458.41"
+       }
+       else {
+         precio = "error"
+       }
+     }
+     }
+   
+     if (lugar === "nacional"){
+      if (reg === "si"){
+       if (km > 0 && km < 3){
+         precio = "$ 715.24";
+       }
+       else if (km > 3 && km < 6) {
+         precio = "794.74"
+       }
+       else if (km > 6 && km < 12) {
+         precio = "855.97"
+       } 
+       else if (km > 12 && km < 27) {
+         precio = "917.24"
+       }
+       else {
+         precio = "error"
+       }
+       }
+     
+       if (reg === "no") {
+         if (km > 0 && km < 3) {
+           precio = "1137.23"
+       }
+       else if (km > 3 && km < 6) {
+         precio = "1263.64"
+         }
+         else if (km > 6 && km < 12) {
+           precio = "1360.99"
+         }
+         else if (km > 12 && km <= 27) {
+           precio = "1458.41"
+         }
+         else {
+           precio = "error"
+         }
+       }
+       }
+     
+
 tarifa.innerText = precio
 });
